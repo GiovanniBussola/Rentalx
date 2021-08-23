c05496e104b334c952237f8ab43e64c0ec69dc1e
Giovanni Bússola <giovanni.sbussola@gmail.com>
2021-08-22 21:52:20 -0300

    feat: added refresh_token

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: "meumd5preferido",
+  secret_refresh_token: "meurefreshtokenmd5preferido",
+  expires_in_token: "15m",
+  expires_refresh_token_days: 30,
+  refresh_expires_in_token: "30d",
+};
